6c19737213af800a8aa3926a7302a52f1a4b1429
Yeojin Kim <bluemeta@gmail.com>
2021-05-08 14:14:25 +0900

    mongodb 저장하는 API 추가

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
+from pymongo import MongoClient
 
 # 플라스크 웹 서버 생성하기
 app = Flask(__name__)
+
+# mongodb 추가
+client = MongoClient('localhost', 27017)
+db = client.get_database('sparta')
+
 
 # API 추가
 @app.route('/', methods=['GET'])  # 데코레이터 문법
 def index():  # 함수 이름은 고유해야 한다
     return render_template('index.html', test='테스트')
 
+
+# 아티클 추가 API
+@app.route('/memo', methods=['POST'])
+def save_memo():
+    form = request.form
+    url_receive = form['url_give']
+    comment_receive = form['comment_give']
+
+    print(url_receive, comment_receive)
+    document = {
+        'url': url_receive,
+        'comment': comment_receive,
+    }
+    db.articles.insert_one(document)
+    return jsonify(
+        {'result': 'success'}
+    )
 
 # app.py 파일을 직접 실행시킬 때 동작시킴
 # 이 코드가 가장 아랫부분
